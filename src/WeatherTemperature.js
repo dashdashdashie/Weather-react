d8b3d4193415f25a0abadb16c88b45434300e81d
dashdashdashie <113038052+dashdashdashie@users.noreply.github.com>
2022-11-19 16:32:59 +0000

    added celsius to fahrenheit and back switch

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+
+const [unit, setUnit] = useState("celsius");
+
+function fahrenheit() {
+    return (props.celsius * 9)/5 + 32;
+}
+
+function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+}
+
+function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+}
+
+if (unit === "celsius") {
+    return ( <span className="WeatherTemperature">
+    <span className="temperature">{Math.round(props.celsius)}</span>
+                    <span className="unit">°C | <a href="/" onClick={showFahrenheit} >°F</a></span>
+                    </span>
+)
+} else {
+    return ( <span className="WeatherTemperature">
+    <span className="temperature">{Math.round(fahrenheit())}</span>
+                    <span className="unit"><a href="/" onClick={showCelsius}>°C</a> | °F</span>
+                    </span>
+)
+}
+
+}
